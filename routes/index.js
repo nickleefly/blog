98097eeff5b01889251e36b64306dcaacd325d43
nickleefly <nickleefly@hotmail.com>
2013-06-02 21:44:12 +0800

    add home page and login

--- routes/index.js
+++ routes/index.js
@@ -5,20 +5,20 @@
 
 module.exports = function(app){
   app.get('/',function(req,res){
-    res.render('index', { title: '主页' });
+    res.render('index', { title: 'home' });
   });
   app.get('/reg',function(req,res){
-    res.render('reg', { title: '注册' });
+    res.render('reg', { title: 'register' });
   });
   app.post('/reg',function(req,res){
   });
   app.get('/login',function(req,res){
-    res.render('login', { title: '登录' });
+    res.render('login', { title: 'login' });
   });
   app.post('/login',function(req,res){
   });
   app.get('/post',function(req,res){
-    res.render('post', { title: '发表' });
+    res.render('post', { title: 'post' });
   });
   app.post('/post',function(req,res){
   });
