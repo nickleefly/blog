aad0cbdfa5e732c6fc72fa91e7474da1da350871
nickleefly <nickleefly@hotmail.com>
2013-06-02 22:58:23 +0800

    add post response

--- routes/index.js
+++ routes/index.js
@@ -3,15 +3,22 @@
  * GET home page.
  */
 var crypto = require('crypto'),
-    User = require('../models/user.js');
+    User = require('../models/user.js'),
+    Post = require('../models/post.js');
 
 module.exports = function(app){
-  app.get('/',function(req,res){
-    res.render('index',{
-      title:'home',
-      user: req.session.user,
-      success:req.flash('success').toString(),
-      error:req.flash('error').toString()
+  app.get('/', function(req,res){
+    Post.get(null, function(err, posts){
+      if(err){
+        posts = [];
+      } 
+      res.render('index',{
+        title: 'home',
+        user: req.session.user,
+        posts: posts,
+        success: req.flash('success').toString(),
+        error: req.flash('error').toString()
+      });
     });
   });
 
@@ -109,7 +116,17 @@ module.exports = function(app){
   });
 
   app.post('/post', checkLogin);
-  app.post('/post',function(req,res){
+  app.post('/post', function(req, res){
+    var currentUser = req.session.user,
+        post = new Post(currentUser.name, req.body.title, req.body.post);
+    post.save(function(err){
+      if(err){
+        req.flash('error', err); 
+        return res.redirect('/');
+      }
+      req.flash('success', 'successful posted!');
+      res.redirect('/');
+    });
   });
 
   app.get('/logout', checkLogin);
