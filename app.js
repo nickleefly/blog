5b53f9e4feb352e9b793cd583724c604d8c8d257
nickleefly <nickleefly@hotmail.com>
2013-06-02 19:53:57 +0800

    add routes(app)

--- app.js
+++ app.js
@@ -5,7 +5,6 @@
 
 var express = require('express')
   , routes = require('./routes')
-  , user = require('./routes/user')
   , http = require('http')
   , path = require('path');
 
@@ -27,9 +26,8 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
-app.get('/', routes.index);
-app.get('/users', user.list);
-
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
+
+routes(app);
