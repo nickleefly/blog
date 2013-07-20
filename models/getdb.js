b8fd99e060488e6c8cb97d10bdc97a43222039f0
nickleefly <nickleefly@gmail.com>
2013-07-20 13:05:47 +0800

    working connection once, use it elsewhere

--- models/getdb.js
+++ models/getdb.js
@@ -1,2 +1,17 @@
 var db = require('./testdb');
-var client = db.get();
+var mongodb = require('mongodb');
+var items;
+db.get(function(client) {
+  items = new mongodb.Collection(client, 'testCollection');
+});
+
+// then anywhere in your code
+db.get(function() {
+  // items.find({ ...
+  items.insert({a:3}, {w:1}, function(err, result) {
+    if(!err) {
+      console.log('inserted')
+      console.log(result);
+    }
+  });
+});
--- models/testdb.js
+++ models/testdb.js
@@ -1,17 +1,29 @@
 var mongodb = require('mongodb'),
+    events = require('events'),
     Db = mongodb.Db,
+    EE = new events.EventEmitter(),
     Connection = mongodb.Connection,
     Server = mongodb.Server,
+    access = new Server('localhost', 27017)
     client = null;
 
-    new Db('blog', new Server('localhost', Connection.DEFAULT_PORT, {auto_reconnect: true})
-    ).open(function (err, c) {
-      if (!err) {
-        client = c;
-        console.log('database connected');
-      } else {
-        console.log('database connection error', err);
-      }
-    });
+new Db('blog', access, { safe: true, auto_reconnect: true }).open(function (err, c) {
+  if (!err) {
+    client = c;
+    console.log('database connected');
+    EE.emit('connect');
+  } else {
+    console.log('database connection error', err);
+    EE.emit('error');
+  }
+});
 
-exports.get = function() { return client; };
+exports.get = function(fn) {
+  if(client) {
+    fn(client);
+  } else {
+    EE.on('connect', function() {
+      fn(client);
+    });
+  }
+};
